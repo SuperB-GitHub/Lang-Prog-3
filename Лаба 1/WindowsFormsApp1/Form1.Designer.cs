0fa5a84cbfeb54b6fdb04f85c05015682324ad64
SuperB-GitHub <mr.covalets2015@yandex.ru>
2024-10-13 21:57:19 +0300

    Добавление 1 лабы

---
+++ Лаба 1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.шоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ниШоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(224, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(377, 126);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Открытие SDI формы";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.шоToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // шоToolStripMenuItem
+            // 
+            this.шоToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ниШоToolStripMenuItem});
+            this.шоToolStripMenuItem.Name = "шоToolStripMenuItem";
+            this.шоToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.шоToolStripMenuItem.Text = "&Шо";
+            // 
+            // ниШоToolStripMenuItem
+            // 
+            this.ниШоToolStripMenuItem.Name = "ниШоToolStripMenuItem";
+            this.ниШоToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ниШоToolStripMenuItem.Text = "&Ни шо";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "MDI";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem шоToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ниШоToolStripMenuItem;
+    }
+}
+
