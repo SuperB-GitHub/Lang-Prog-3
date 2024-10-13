832fc7114c856bbaa5178857705d1edf8d5e4a1e
SuperB-GitHub <148558050+SuperB-GitHub@users.noreply.github.com>
2024-10-13 22:19:54 +0300

    Добавление 1 лабы

---
+++ Лаба 1/Second Example/Form1.cs
@@ -0,0 +1,27 @@
+namespace Second_Example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        protected void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath myPath = new
+            System.Drawing.Drawing2D.GraphicsPath();
+            myPath.AddPolygon(new Point[] { new Point(0, 0), new Point(0, this.Height),
+            new Point(this.Width, 0) });
+            Region myRegion = new Region(myPath); this.Region = myRegion;
+
+
+        }
+    }
+}
