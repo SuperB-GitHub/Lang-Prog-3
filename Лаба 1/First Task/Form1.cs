87453724b8ef7ddfe470e1961ae912486cebfc21
SuperB-GitHub <148558050+SuperB-GitHub@users.noreply.github.com>
2024-10-13 22:17:57 +0300

    Добавление 1 лабы

---
+++ Лаба 1/First Task/Form1.cs
@@ -0,0 +1,23 @@
+namespace First_Task
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath myPath = new System.Drawing.Drawing2D.GraphicsPath();
+            myPath.AddEllipse(0, 0, this.Width, this.Height);
+            Region myRegion = new Region(myPath); this.Region = myRegion;
+
+        }
+    }
+}
